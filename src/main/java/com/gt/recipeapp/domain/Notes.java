9ba40b7d0d5698a62c75a5eaff939b35233355a4
gt <gt@arch.com>
2022-03-21 16:04:52 +0300

    Add LICENSE

---
+++ src/main/java/com/gt/recipeapp/domain/Notes.java
@@ -0,0 +1,9 @@
+package com.gt.recipeapp.domain;
+
+/*
+    Created by gt at 10:45 PM on Wednesday, August 18, 2021.
+    Project: recipeapp, Package: com.gt.recipeapp.domain.
+*/
+    
+    public class Notes {
+}
---
+++ src/main/java/com/gt/recipeapp/domain/Recipe.java
@@ -0,0 +1,9 @@
+package com.gt.recipeapp.controllers.domain;
+
+/*
+    Created by gt at 10:39 PM on Wednesday, August 18, 2021.
+    Project: recipeapp, Package: com.gt.recipeapp.controllers.domain.
+*/
+
+public class Recipe {
+}
